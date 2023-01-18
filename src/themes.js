4c20187bc6e89409bf84847c0fa046122873e1a1
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2023-01-18 22:29:11 +0900

    :lipstick: Add themes

---
+++ src/themes.js
@@ -0,0 +1,56 @@
+export let themes = {
+  light: {
+    border_color: "#d5d8da",
+    background_color: "#f6f8fa",
+    language_color: "#24292f",
+    percent_color: "#57606a",
+  },
+  light_high_contrast: {
+    border_color: "#2f3237",
+    background_color: "#e7ecf0",
+    language_color: "#0e1116",
+    percent_color: "#0e1116",
+  },
+  light_colorblind: {
+    border_color: "#d5d8da",
+    background_color: "#f6f8fa",
+    language_color: "#24292f",
+    percent_color: "#57606a",
+  },
+  light_tritanopia: {
+    border_color: "#d5d8da",
+    background_color: "#f6f8fa",
+    language_color: "#24292f",
+    percent_color: "#57606a",
+  },
+  dark: {
+    border_color: "#363b42",
+    background_color: "#21262d",
+    language_color: "#c9d1d9",
+    percent_color: "#8b949e",
+  },
+  dark_dimmed: {
+    border_color: "#474e57",
+    background_color: "#373e47",
+    language_color: "#adbac7",
+    percent_color: "#768390",
+  },
+  dark_high_contrast: {
+    border_color: "#7a828e",
+    background_color: "#272b33",
+    language_color: "#f0f3f6",
+    percent_color: "#f0f3f6",
+  },
+  dark_colorblind: {
+    border_color: "#363b42",
+    background_color: "#21262d",
+    language_color: "#c9d1d9",
+    percent_color: "#8b949e",
+  },
+  dark_tritanopia: {
+    border_color: "#363b42",
+    background_color: "#21262d",
+    language_color: "#c9d1d9",
+    percent_color: "#8b949e",
+  },
+};
