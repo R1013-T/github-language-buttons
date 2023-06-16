18fb56b4848958f5af385a043ae3e7d8ecd49b5a
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2023-06-16 11:07:40 +0900

    :wrench: Edit cache control

--- api/bar.js
+++ api/bar.js
@@ -3,11 +3,11 @@ import { createBar } from "../src/bar.js";
 export default async function handler(request, response) {
   try {
     response.setHeader("Content-Type", "image/svg+xml");
-    response.setHeader("Cache-Control", "max-age=0, s-maxage=86400");
+    response.setHeader("Cache-Control", "max-age=3600");
     response.send(await createBar(request.query.width));
   } catch (error) {
     response.setHeader("Content-Type", "text/plain");
-    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    response.setHeader("Cache-Control", "no-store");
     response.send(error.message);
   }
 }
--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ import { createButton } from "../src/button.js";
 export default async function handler(request, response) {
   try {
     response.setHeader("Content-Type", "image/svg+xml");
-    response.setHeader("Cache-Control", "max-age=0, s-maxage=86400");
+    response.setHeader("Cache-Control", "max-age=3600");
     response.send(await createButton(request.query.language, request.query.theme || "dark"));
   } catch (error) {
     response.setHeader("Content-Type", "text/plain");
-    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    response.setHeader("Cache-Control", "no-store");
     response.send(error.message);
   }
 }
